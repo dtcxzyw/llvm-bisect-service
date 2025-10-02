e53ddd2efb3ca33c47eb3da8fcdcf6758f1b688b
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-10-03 02:21:53 +0800

    Fix metadata downloading

--- sync-manyclangs.py
+++ sync-manyclangs.py
@@ -6,13 +6,15 @@ MANYCLANGS_META = MANGCLANGS_URL + "aarch64-ubuntu2004.esi"
 MANYCLANGS_LOCAL = "work/manyclangs/elfshaker_data/packs"
 
 os.makedirs(MANYCLANGS_LOCAL, exist_ok=True)
-subprocess.check_call(["wget", MANYCLANGS_META], cwd=MANYCLANGS_LOCAL)
+subprocess.check_call(
+    ["wget", MANYCLANGS_META, "-O", "aarch64-ubuntu2004.esi"], cwd=MANYCLANGS_LOCAL
+)
 with open(os.path.join(MANYCLANGS_LOCAL, "aarch64-ubuntu2004.esi"), "r") as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip()
         if line.endswith(".pack"):
-            filename = line[line.rindex("\t") + 1:]
+            filename = line[line.rindex("\t") + 1 :]
             if not os.path.exists(os.path.join(MANYCLANGS_LOCAL, filename)):
                 print(f"Downloading {filename}")
                 subprocess.check_call(
